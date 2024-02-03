22d8fa1d88cce08d38b64a1c8df92949cf3cd02d
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2024-02-03 13:54:09 +0200

    cores allows

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const PORT=process.env.PORT || 5000
+const cors = require('cors');
 const express= require("express")
 const app =express()
 const {errorHandler}=require('./middleware/errorMiddleware')
@@ -10,19 +11,24 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 connectDB()
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://65be266999ee797ffe7e996c--jade-frangipane-0df171.netlify.app");
-    // Add other necessary headers here
-    // Set the allowed headers, including 'Content-Type'
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+app.use(cors({
+    origin: 'https://65be266999ee797ffe7e996c--jade-frangipane-0df171.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://65be266999ee797ffe7e996c--jade-frangipane-0df171.netlify.app");
+//     // Add other necessary headers here
+//     // Set the allowed headers, including 'Content-Type'
+//     res.header("Access-Control-Allow-Headers", "Content-Type");
   
-    // Allow the methods you need
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     // Allow the methods you need
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   
-    // Allow credentials if necessary
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
+//     // Allow credentials if necessary
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+//   });
 //Routes
 app.use('/api/users', require('./Routes/userRoutes'))
 app.use('/api/incomes', require('./Routes/incomeRoutes'))
